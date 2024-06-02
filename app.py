42cbc37d2d3ac61425b6d03d1b661e1279146aea
paraspoudel72 <pas076bct020@wrc.edu.np>
2024-06-02 20:12:19 +0545

    streamlit and pdf extractor

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
 # import huggingface_hub as 
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
 
 
 
@@ -14,8 +24,17 @@ def main():
 
     with st.sidebar:
         st.subheader('Your documents Please')
-        st.file_uploader("Upload your pdf's here please")
-        st.button("Provide")
+        pdf_docs = st.file_uploader("Upload your pdf's here please", accept_multiple_files= True)
+        if st.button("Provide"):
+            with st.spinner('Processing...'):
+                # get pdf text
+                raw_text = get_pdf_text(pdf_docs)
+            
+
+                # get the text chunks
+
+
+                #create vector store
 
 
 if __name__ == '__main__':
